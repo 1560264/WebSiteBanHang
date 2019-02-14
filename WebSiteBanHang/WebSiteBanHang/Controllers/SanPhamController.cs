8e65d23f537b560b7cf4559a42d4103e2bd2d053
DangKhoaDo <32297622+1560264@users.noreply.github.com>
2019-02-14 18:27:59 +0700

    NewProject

---
+++ WebSiteBanHang/WebSiteBanHang/Controllers/SanPhamController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteBanHang.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        //
+        // GET: /SanPham/
+        public ActionResult SanPham1()
+        {
+            return View();
+        }
+        public ActionResult SanPham2()
+        {
+            return View();
+        }
+        public ActionResult SanPhamPartial()
+        {
+            return PartialView();
+        }
+	}
+}
